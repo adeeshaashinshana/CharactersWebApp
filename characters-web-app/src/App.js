d38444795f3873f8e7e7d552760cc3473e95151a
adeesha.nichelabs <adeesha.mahalekam@nichelabs.lk>
2022-04-21 01:04:49 +0530

    set basic project structure

---
+++ characters-web-app/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import LoginPage from "../src/Pages/LoginPage";
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <LoginPage />
+      </header>
+    </div>
+  );
+};
+
+export default App;
---
+++ characters-web-app/src/Pages/LoginPage.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const LoginPage = () => {
+  return (
+    <div className="card">
+      <div id="card-content">
+        <div id="card-title">
+          <h2>LOGIN</h2>
+          <div className="underline-title"></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LoginPage;
